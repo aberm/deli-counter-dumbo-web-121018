fd10ade6d4f6791bd32f77a969a7840a0acbf2a7
Menachem Aber <abermendel@gmail.com>
2018-11-23 02:41:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@
 def line(line_array)
   if line_array.length == 0
     puts "The line is currently empty."
-  end
-  str = "The line is currently:"
-  for i in (0..line_array.length) do
-    str += " #{i+1}. #{line_array[i]}"
-  end
-  puts str
+  else
+    str = "The line is currently:"
+    for i in (0..line_array.length) do
+      str += " #{i+1}. #{line_array[i]}"
+    end
+    puts str
 end
