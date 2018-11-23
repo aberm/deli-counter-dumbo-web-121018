50e8a7f4df3a5ed0b4468ce0bb04bcb45eefdd92
Menachem Aber <abermendel@gmail.com>
2018-11-23 02:37:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
 
 def line(line_array)
+  if line_array.length == 0
   str = "The line is currently:"
   for i in (0..line_array.length) do
     str += " #{i+1}. #{line_array[i]}"
   end
+  str
 end
