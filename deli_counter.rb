8de9393033369bd2086b15062977d36d4670b264
Menachem Aber <abermendel@gmail.com>
2018-11-23 02:39:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@
 def line(line_array)
   if line_array.length == 0
     "The line is currently empty."
+  end
   str = "The line is currently:"
   for i in (0..line_array.length) do
     str += " #{i+1}. #{line_array[i]}"
